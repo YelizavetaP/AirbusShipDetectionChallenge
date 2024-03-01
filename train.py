1242f45dc19399a7b0ba188b54237565a122205e
Yelyzaveta <liz47220@gmail.com>
2024-03-01 03:04:52 +0200

    finished analysis

--- train.py
+++ train.py
@@ -3,11 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns        
+from sklearn.model_selection import train_test_split
 
 from skimage.io import imread                        
 from skimage.segmentation import mark_boundaries     #mark bounds around shapes
 from skimage.util import montage                     #create montage od imgs
 from skimage.morphology import label                 
+
+
 
 
 TRAIN_IMAGE_DIR = "../data/train_v2"
@@ -19,21 +22,64 @@ print('Test train: \n', train[:5])
 print('Test masks: \n',masks.head())
 
 
+
+
 # Add extra col to mark if there is a ship
 masks['ships'] = masks['EncodedPixels'].map(lambda c_row: 1 if isinstance(c_row, str) else 0)
 print('Test ships: \n',masks.head(9))
-
 # Sum up ship counts
 unique_img_ids = masks.groupby('ImageId').agg({'ships': 'sum'}).reset_index() 
 unique_img_ids.index+=1 # Incrimenting all the index by 1
 print('Test unique imgs: \n',unique_img_ids.head())
-
 # Mark if ship exist on unique imgs
 unique_img_ids['has_ship'] = unique_img_ids['ships'].map(lambda x: 1.0 if x>0 else 0.0)
 print('Test unique imgs ships: \n',unique_img_ids.head())
-
 # add file size
 unique_img_ids['file_size_kb'] = unique_img_ids['ImageId'].map(lambda c_img_id: os.stat(os.path.join(TRAIN_IMAGE_DIR, c_img_id)).st_size/1024)
 print('Test file size: \n',unique_img_ids.head())
+# remove 'ships' from original masks
+unique_img_ids = unique_img_ids[unique_img_ids.file_size_kb>35]
+unique_img_ids.head()
+
+
+
+
+# split into train and test 
+# stratify - same proportions for train and test
+train_ids, valid_ids = train_test_split(unique_img_ids, test_size=0.3, stratify=unique_img_ids['ships'])
+
+train_df = pd.merge(masks, train_ids)
+valid_df = pd.merge(masks, valid_ids)
+print('Train count: ', train_df.shape[0])
+print('Train count: ', valid_df.shape[0])
+
+
+
+
+
+# NEED HERE:
+# split function
+# undersampling function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
